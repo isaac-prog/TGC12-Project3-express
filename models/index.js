8e276686558af5146020bb5147db5588d2a0c843
isaac-prog <panda_mm88@yahoo.com>
2021-07-19 10:05:07 +0000

    added basics

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const bookshelf = require('../bookshelf')
+
+const Product = bookshelf.model('Product', {
+    tableName:'products'
+});
+
+const Category = bookshelf.model('Category',{
+    tableName: 'categories'
+})
+
+module.exports = { Product };
---
+++ routes/landing.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router(); // #1 - Create a new express Router
+
+router.get('/', (req,res)=>{
+    res.render('landing/index')
+})
+
+router.get('/about-us', (req,res)=>{
+    res.render('landing/about-us')
+})
+
+router.get('/contact-us', (req,res)=>{
+    res.render('landing/contact-us')
+})
+
+module.exports = router; // #3 export out the router
